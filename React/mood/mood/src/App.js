713b2522230386ddc496322224880e2f697927e6
geunpyochoi <ckp220@kookmin.ac.kr>
2023-10-09 18:24:23 +0900

    mood folder update

---
+++ React/mood/mood/src/App.js
@@ -0,0 +1,10 @@
+import MenuList from "./components/MenuList/MenuList";
+function App() {
+  return (
+    <div className="title">
+      <h1>오늘의 기분을 선택해주세요. 😀</h1>
+      <MenuList/>
+    </div>
+  );
+}
+export default App;
---
+++ React/mood/mood/src/components/MenuList/MenuList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MenuList() {
+  const menus = ["좋아요! 😃", "정말 좋아요! 🤭", "최고에요! 😄", "미쳤어요!! 🤪"];
+  return (
+    <div>
+      
+    </div>
+  )
+}
