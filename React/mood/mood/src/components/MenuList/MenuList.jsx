69596b42560bf3e8e7105944eb4940d019e5bf2c
geunpyochoi <ckp220@kookmin.ac.kr>
2023-10-11 17:11:21 +0900

    init setting

--- React/mood/mood/src/components/MenuList/MenuList.jsx
+++ React/mood/mood/src/components/MenuList/MenuList.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function MenuList() {
-  const menus = ["좋아요! 😃", "정말 좋아요! 🤭", "최고에요! 😄", "미쳤어요!! 🤪"];
-  return (
-    <div>
-      
-    </div>
-  )
+  const menus = [
+    "좋아요! 😃",
+    "정말 좋아요! 🤭",
+    "최고에요! 😄",
+    "미쳤어요!! 🤪",
+  ];
+  return <div></div>;
 }
