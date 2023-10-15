e6796ab11bc10fe5c3f5b9fb8d6eb9c5aa28e319
geunpyochoi <ckp220@kookmin.ac.kr>
2023-10-15 12:46:40 +0900

    Baekjoon2751,10815

---
+++ codingTestNote.py
@@ -0,0 +1,22 @@
+# 백준 2751
+# import sys
+# n = int(input())
+# a = []
+# for _ in range(n):
+#   a.append(int(sys.stdin.readline()))
+# a.sort()
+# for i in a:
+#   print(i)
+
+import sys
+n = int(sys.stdin.readline())
+a = list(map(int,sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+b = list(map(int,sys.stdin.readline().split()))
+ans = [0 for _ in range(m)]
+for i in a:
+  for j in range(m):
+    if i == b[j]:
+      ans[j] = 1
+      break
+print(ans)
