bc32022ee84face56359fc49fdc21d13ba686d1a
geunpyochoi <ckp220@kookmin.ac.kr>
2023-11-20 21:21:47 +0900

    git_ignore update

--- codingTestNote.py
+++ codingTestNote.py
@@ -8,15 +8,25 @@
 # for i in a:
 #   print(i)
 
-import sys
-n = int(sys.stdin.readline())
-a = list(map(int,sys.stdin.readline().split()))
-m = int(sys.stdin.readline())
-b = list(map(int,sys.stdin.readline().split()))
-ans = [0 for _ in range(m)]
-for i in a:
-  for j in range(m):
-    if i == b[j]:
-      ans[j] = 1
-      break
-print(ans)
+# import sys
+# n = int(sys.stdin.readline())
+# a = list(map(int,sys.stdin.readline().split()))
+# m = int(sys.stdin.readline())
+# b = list(map(int,sys.stdin.readline().split()))
+# ans = [0 for _ in range(m)]
+# for i in a:
+#   for j in range(m):
+#     if i == b[j]:
+#       ans[j] = 1
+#       break
+# print(ans)
+
+
+# 백준 27433
+# n = int(input())
+# def factorial(num):
+#   if num > 0:
+#     return num * factorial(num-1)
+#   else:
+#     return 1
+# print(factorial(n))
