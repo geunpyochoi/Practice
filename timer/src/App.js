252d1db08f302ce72585067fa3ac78c1adf0c39f
geunpyochoi <ckp220@kookmin.ac.kr>
2023-11-28 21:43:28 +0900

    뽀모도로 타이머 완성

--- timer/src/App.js
+++ timer/src/App.js
@@ -14,62 +14,71 @@ const formatNumber = (number) => {
 
 function App() {
   const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(24);
+  const [minutes, setMinutes] = useState(25);
   const [startBtn, setStartBtn] = useState(true);
   const [step, setStep] = useState(1);
   const [timerRunning, setTimerRunning] = useState(false);
   const [isBreakTime, setIsBreakTime] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (timerRunning) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 0) {
+            setMinutes((prevMinutes) => prevMinutes - 1);
+            return 59;
+          } else {
+            return prevSeconds - 1;
+          }
+        });
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timerRunning]);
+
+  useEffect(() => {
+    if (minutes === 0 && seconds === 0) {
+      if (isBreakTime) {
+        // 쉬는 시간이 끝났을 때 초기화 및 작업 단계 증가
+        setMinutes(25);
+        setSeconds(0);
+        setStep((prevStep) => prevStep + 1);
+        setIsBreakTime(false);
+      } else {
+        // 작업 시간이 끝났을 때 쉬는 시간 설정
+        if (step % 4 === 0) {
+          // step이 4의 배수일 때
+          setMinutes(29); // 30분 쉬는 시간 설정
+        } else {
+          setMinutes(4); // 기본 작업 시간
+        }
+        setSeconds(59);
+        setIsBreakTime(true);
+      }
+    }
+
+    if (minutes === 30 && seconds === 0) {
+      // 쉬는 시간이 30분일 때 초기화
+      setMinutes(0);
+      setSeconds(0);
+      setIsBreakTime(false);
+    }
+  }, [seconds, minutes, isBreakTime, step]);
 
   const startTimer = () => {
     setStartBtn((prevStartBtn) => !prevStartBtn);
     setTimerRunning((prevTimerRunning) => !prevTimerRunning);
   };
 
-  useEffect(() => {
-    if (timerRunning) {
-      const interval = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds + 1);
-      }, 1000);
-
-      return () => {
-        clearInterval(interval);
-      };
-    }
-  }, [timerRunning]);
-
-  useEffect(() => {
-    if (seconds === 60) {
-      setMinutes((prevMinutes) => prevMinutes + 1);
-      setSeconds(0);
-    }
-
-    if (minutes === 25) {
-      if (isBreakTime) {
-        // 쉬는 시간이 끝났을 때 초기화 및 작업 단계 증가
-        setMinutes(0);
-        setSeconds(0);
-        setStep((prevStep) => prevStep + 1);
-        setIsBreakTime(false);
-      } else {
-        // 작업 시간이 끝났을 때 쉬는 시간 설정
-        setMinutes(0);
-        setSeconds(0);
-        setIsBreakTime(true);
-      }
-    }
-
-    if (minutes === 5 && isBreakTime) {
-      // 쉬는 시간이 5분일 때 초기화
-      setMinutes(0);
-      setSeconds(0);
-      setIsBreakTime(false);
-    }
-  }, [seconds, minutes, isBreakTime]);
-
   const resetTimer = () => {
     const shouldReset = window.confirm("타이머를 초기화하시겠습니까?");
     if (shouldReset) {
-      setMinutes(0);
+      setMinutes(25);
       setSeconds(0);
       setStep(1);
       setIsBreakTime(false);
@@ -115,7 +124,9 @@ function App() {
             </button>
           </Buttons>
           <Step>
-            <p>STEP {step}</p>
+            <p>
+              STEP {step} {isBreakTime ? ": 쉬는 시간!" : ""}
+            </p>
           </Step>
         </TimerBox>
       </Body>
