589170b94a2246a51d3d655b2a7408262d291a8c
geunpyochoi <ckp220@kookmin.ac.kr>
2023-12-07 22:27:33 +0900

    ts버전 거의 완성

--- timer-ts/src/App.tsx
+++ timer-ts/src/App.tsx
@@ -1,11 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Body, Buttons, Step, TimerBox } from "./AppStyle";
+import { GlobalStyles } from "./AppStyle";
+import start from "./images/button/start-default.png";
+import reset from "./images/button/reset-default.png";
+import pause from "./images/button/pause.png";
+import box from "./images/box.png";
+
+const formatNumber = (number:number) : string|number => {
+  return number < 10 ? `0${number}` : `${number}`;
+};
 
 const App:React.FC = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(25);
+  const [startBtn, setStartBtn] = useState(true);
+  const [step, setStep] = useState(1);
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [isBreakTime, setIsBreakTime] = useState(false);
+
+  useEffect(() => {
+    let interval:NodeJS.Timer;
+
+    if (timerRunning) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 0) {
+            setMinutes((prevMinutes) => prevMinutes - 1);
+            return 59;
+          } else {
+            return prevSeconds - 1;
+          }
+        });
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timerRunning]);
+
+  useEffect(() => {
+    if (minutes === 0 && seconds === 0) {
+      if (isBreakTime) {
+        // 쉬는 시간이 끝났을 때 초기화 및 작업 단계 증가
+        setMinutes(25);
+        setSeconds(0);
+        setStep((prevStep) => prevStep + 1);
+        setIsBreakTime(false);
+      } else {
+        // 작업 시간이 끝났을 때 쉬는 시간 설정
+        if (step % 4 === 0) {
+          // step이 4의 배수일 때
+          setMinutes(29); // 30분 쉬는 시간 설정
+        } else {
+          setMinutes(4); // 기본 쉬는 시간
+        }
+        setSeconds(59);
+        setIsBreakTime(true);
+      }
+    }
+
+    if (minutes === 30 && seconds === 0) {
+      // 쉬는 시간이 30분일 때 초기화
+      setMinutes(0);
+      setSeconds(0);
+      setIsBreakTime(false);
+    }
+  }, [seconds, minutes, isBreakTime, step]);
+
+  const startTimer = () => {
+    setStartBtn((prevStartBtn) => !prevStartBtn);
+    setTimerRunning((prevTimerRunning) => !prevTimerRunning);
+  };
+
+  const resetTimer = () => {
+    const shouldReset = window.confirm("타이머를 초기화하시겠습니까?");
+    if (shouldReset) {
+      setMinutes(25);
+      setSeconds(0);
+      setStep(1);
+      setIsBreakTime(false);
+      setTimerRunning(false);
+      setStartBtn(true);
+    }
+  };
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <GlobalStyles />
+      <Body>
+        <p className='logo'>Pomodoro</p>
+        <TimerBox>
+          <p className='timer'>TIMER</p>
+          <hr />
+          <div className='time-text'>
+            <div className='time min'>
+              <div className='dot'>
+                <p>MIN</p>
+                <img src={box} alt='' className='box' />
+              </div>
+              <div className='minute'>
+                <p>{formatNumber(minutes)}</p>
+              </div>
+            </div>
+            <div className='time sec'>
+              <div className='dot'>
+                <p>SEC</p>
+                <img src={box} alt='' className='box' />
+              </div>
+              <div className='second'>
+                <p>{formatNumber(seconds)}</p>
+              </div>
+            </div>
+          </div>
+          <Buttons>
+            <button onClick={startTimer}>
+              <img src={startBtn ? start : pause} alt='' />
+            </button>
+            <button onClick={resetTimer}>
+              <img src={reset} alt='' />
+            </button>
+          </Buttons>
+          <Step>
+            <p>
+              STEP {step} {isBreakTime ? ": 쉬는 시간!" : ""}
+            </p>
+          </Step>
+        </TimerBox>
+      </Body>
+    </>
   );
 }
 
---
+++ timer-ts/src/AppStyle.tsx
@@ -0,0 +1,136 @@
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import dot from "./images/dot.png";
+
+export const GlobalStyles = createGlobalStyle`
+  ${reset}
+  @font-face {
+    font-family: 'GongGothicMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-10@1.0/GongGothicMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+`;
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: var(
+    --gradient2,
+    radial-gradient(293.21% 130.49% at 3.59% 5.28%, #f0f4ff 0%, #a5b6ee 100%)
+  );
+  height: 100vh;
+  width: 100vw;
+  .logo {
+    color: #374b7c;
+    font-family: "GongGothicMedium";
+    margin-top: 130px;
+    font-size: 50px;
+    font-weight: 700;
+  }
+`;
+
+export const TimerBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 600px;
+  height: 500px;
+  flex-shrink: 0;
+  background: var(
+    --gradient,
+    linear-gradient(144deg, #5572bb 2.13%, #374b7c 102.97%)
+  );
+  border-radius: 16px;
+  margin-top: 60px;
+  .timer {
+    font-size: 30px;
+    margin-top: 60px;
+    font-family: "GongGothicMedium";
+    color: #fff;
+    font-weight: 700;
+  }
+  hr {
+    margin-top: 40px;
+    width: 80%;
+    background: #3d538a;
+    border: 0;
+    height: 1px;
+  }
+  .time-text {
+    width: 100%;
+    font-family: "Pretendard-Regular";
+    color: #fff;
+    display: flex;
+    justify-content: center;
+  }
+  .time {
+    display: flex;
+    align-items: center;
+    position: relative;
+  }
+  .dot {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 40px;
+    p {
+      color: var(--light, #a5b6ee);
+      margin-bottom: 10px;
+    }
+  }
+  .hrs::after,
+  .min::after {
+    content: "";
+    display: block;
+    width: 100px;
+    height: 100px;
+    background: url(${dot}) no-repeat center 70px;
+  }
+  .minute {
+    font-size: 70px;
+    position: absolute;
+    top: 48%;
+    left: 11%;
+  }
+  .second {
+    text-align: center;
+    font-size: 70px;
+    position: absolute;
+    top: 48%;
+    left: 19%;
+  }
+
+  .box {
+    width: 130px;
+    height: 130px;
+  }
+`;
+export const Buttons = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  button {
+    cursor: pointer;
+    border: 0;
+    background-color: transparent;
+  }
+`;
+
+export const Step = styled.div`
+  margin-top: 20px;
+  p {
+    font-size: 2em;
+    color: #ffffff;
+    font-family: "GongGothicMedium";
+  }
+`;
